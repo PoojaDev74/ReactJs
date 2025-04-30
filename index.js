452e14fa6094e56a31cfc8dd21323b5e2054ae08
PoojaDev74 <poojapreethi2001@gmail.com>
2025-04-30 21:08:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './App.css'
+// require("dotenv").config();
+//import { Router } from "express";
+const express = require("express");
 
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const cors = require("cors");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Port
+const port = process.env.PORT || 4000
+// db connection
+require(`./db/connection`);
 
+app.get("/", (req,res) => {
+    res.send("hello!")
+});
+
+// Require routes
+const groupRoutes = require(`./routes/groupRoute`);
+const noteRoutes = require(`./routes/notesRoute`);
+
+// MiddleWares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/group",groupRoutes);
+app.use("/api/notes", noteRoutes);
+
+app.listen(port, ()=>{
+    console.log(`Server is running at PORT: ${port}`);
+});
