ec219920221cf530635c88b45278e909e81a4372
Pooja D <poojapreethi2001@gmail.com>
2025-07-04 21:43:44 +0530

    Update api.js

--- Front end/api.js
+++ Front end/api.js
@@ -1,35 +1,40 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "http://localhost:4000/api",
-// });
-
 const BASE_URL = 'http://localhost:4000/api';
 
-export const fetchGroup = async () => {
-  const res = await axios.get(`${BASE_URL}/group`);
-  return res.data;
+export const fetchGroups = async () => {
+  const response = await axios.get(`${BASE_URL}/groups`);
+  return response.data;
 };
 
-export const createGroup = async (name) => {
-  await axios.post(`${BASE_URL}/group`, { name });
+export const createGroup = async (group) => {
+  // const initial = name.charAt(0).toUpperCase(); // first letter
+  // const color = getRandomColor();
+  const response = await axios.post(`${BASE_URL}/groups`, group);
+  return response.data;
+};
+
+export const deleteGroup = async (groupId) => {
+  const response = await axios.delete(`${BASE_URL}/groups/${groupId}`);
+  return response.data;
 };
 
 export const fetchNotes = async (groupId) => {
-  const res = await axios.get(`${BASE_URL}/notes/${groupId}`);
-  return res.data;
+  const response = await axios.get(`${BASE_URL}/notes/${groupId}`);
+  return response.data;
 };
 
-export const createNote = async (groupId, content) => {
-  await axios.post(`${BASE_URL}/notes`, { groupId, content });
+export const createNote = async (groupId, note) => {
+  const response = await axios.post(`${BASE_URL}/notes/${groupId}`, note);
+  return response.data;
 };
 
-// export const getGroups = () => API.get("/groups");
-// export const createGroup = (name) => API.post("/groups", { name });
+export const deleteNote = async (noteId) => {
+  const response = await axios.delete(`${BASE_URL}/notes/note/${noteId}`);
+  return response.data;
+};
 
-// export const getNotes = (groupId) => API.get(`/notes/${groupId}`);
-// export const createNote = (data) => API.post("/notes", data);
-// export const updateNote = (id, content) => API.put(`/notes/${id}`, { content });
-// export const deleteNote = (id) => API.delete(`/notes/${id}`);
-
-// export default API;
+export const updateNote = async (noteId, updatedNote) => {
+  const response = await axios.put(`${BASE_URL}/notes/note/${noteId}`, updatedNote);
+  return response.data;
+};
